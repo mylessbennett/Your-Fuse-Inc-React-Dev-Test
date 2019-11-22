626eff5d7e175c42088956905de8090e1e63ba1a
Myles Bennett <mylessbennett@gmail.com>
2019-11-22 16:30:30 -0500

    remove import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './components/NavBar'
 import CryptoGrid from './components/CryptoGrid'
+import CryptoList from './components/CryptoList'
 import './App.css';
 
 function App() {
 
+  const [view, setView] = useState('grid')
+
   return (
     <div>
       <NavBar />
-      <CryptoGrid />
+      {view === 'grid' ? (<CryptoGrid />) : (<CryptoList />) }
+      
     </div>
   );
 }
